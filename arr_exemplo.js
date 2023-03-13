d4e5da79d398bb98fcafbb596096bd71ddc83e86
Carlos <silva@edu.sc.senai.br>
2023-03-12 21:19:56 -0300

    adiocionando funções JS

--- arr_exemplo.js
+++ arr_exemplo.js
@@ -12,3 +12,25 @@ function excluir(){
     nome_cidade.pop()
     res.innerHTML = "As cidades inseridas são: " + nome_cidade
 }
+
+function inserir_inicio(){
+    let cidade = document.getElementById('cidade').value
+    nome_cidade.unshift(cidade)
+    res.innerHTML = 'As cidades inseridas são: '+nome_cidade    
+}
+
+function excluir_inicio(){
+    let cidade = document.getElementById('cidade').value
+    nome_cidade.shift()
+    res.innerHTML = 'As cidades inseridas são: '+nome_cidade    
+}
+
+function ordenar(){
+    nome_cidade.sort()
+    res.innerHTML = 'As cidades em ordem são: '+nome_cidade    
+}
+
+function reverter(){
+    nome_cidade.reverse()
+    res.innerHTML = 'As cidades em ordem reversa são: '+nome_cidade    
+}
